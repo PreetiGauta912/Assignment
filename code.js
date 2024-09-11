250d94c1e9f99f599adabc46f54114e49f4f1f8c
PreetiGautam <pg973632@gmail.com>
2024-09-11 09:26:54 +0530

    added

---
+++ code.js
@@ -0,0 +1,13 @@
+let a=4;
+let b=5;
+let c=10;
+if(a>b&& b>c){
+    console.log(' a is greater'+ a );
+
+}
+else if(b>c&&c>a){
+    console.log('b is greater'+ b);
+}
+else {
+    console.log('c is greater'+ c);
+}
